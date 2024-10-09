f455f0d9c609aacdc6393b848a8cf89b9d435c16
Gabriel Moretto <gabriel.moretto@universo.univates.br>
2024-10-09 06:25:24 -0300

    Inserindo novos arquivos

---
+++ modulo2/classes/ContaCorrente.php
@@ -0,0 +1,25 @@
+<?php 
+class ContaCorrente extends Conta
+{
+    protected $limite;
+
+    public function __construct($agencia,$conta, $saldo, $limite)
+    {
+        parent::__construct($agencia,$conta, $saldo);
+        $this->limite = $limite;
+    }
+
+    public function retirar($quantia)
+    {
+        if (($this->saldo + $this->limite) >= $quantia) 
+        {
+            $this->saldo -= $quantia;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+?>
